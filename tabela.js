ae00c70e09f63f68aec18ad02303fc8e9f72cb13
Gillian619 <alenacard619@gmail.com>
2024-09-09 11:50:53 -0300

    commit

--- tabela.js
+++ tabela.js
@@ -33,10 +33,30 @@ console.log(linesToColumns(["nome;idade", "João;28", "Maria;32"]))
 console.log(linesToColumns(["produto;preço", "Arroz;5.00", "Feijão;7.50"]))
 console.log(linesToColumns(["Filme;Diretor;Ano;Duração;Gênero;País de Origem;Receita Bruta", "Parasita;Bong Joon-ho;2019;132;Drama;Coréia do Sul;258.8M", "Green Book;Peter Farrelly;2018;130;Drama;EUA;321.8M"]))
 function extractHeader(arr) {
-    header = arr.shift()
+    let header = arr.shift()
     return header
 }
 console.log(extractHeader([["nome", "idade"], ["João", "28"], ["Maria", "32"]]))
 console.log(extractHeader([["produto", "preço"], ["Arroz", "5.00"], ["Feijão", "7.50"]]))
 console.log(extractHeader([["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem",
     "Receita Bruta"], ["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"], ["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA", "321.8M"]]))
+function extractContent(arr) {
+    arr.slice(1)
+    return arr
+}
+console.log(extractContent([["nome", "idade"], ["João", "28"], ["Maria", "32"]]))
+console.log(extractContent([["produto", "preço"], ["Arroz", "5.00"], ["Feijão", "7.50"]]))
+console.log(extractContent([["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"], ["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"], ["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA", "321.8M"]]))
+
+function rowToJSON(header, content) {
+    const obj = {};
+    for (let i = 0; i < header.length; i++) {
+        let key = header[i]
+        let value = content[i]
+        obj[key] = value
+    }
+    return obj
+}
+console.log(rowToJSON(["nome", "idade"], ["João", "28"]))
+console.log(rowToJSON(["produto", "preço"], ["Arroz", "5.00"]))
+console.log(rowToJSON(["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"], ["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"]))

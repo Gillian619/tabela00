41bd35f780557fc3cf68f3367f0573a18be00956
Gillian619 <alenacard619@gmail.com>
2024-09-11 08:54:55 -0300

    commit final

--- tabela.js
+++ tabela.js
@@ -3,13 +3,13 @@
     const csvOscar = fs.readFileSync(arc, 'utf-8')
     return csvOscar
 }*/
-/*function extract() {
+function extract() {
     const fs = require('fs')
     const csvOscar = fs.readFileSync('oscar_best_pictures.csv', 'utf-8')
     return csvOscar
 }
 console.log(extract())
-*/
+
 
 function csvToLines(str) {
     let line = str.split('\r\n')
@@ -78,3 +78,5 @@ console.log("teste 6")
 console.log(contentToJsonData(["nome", "idade"], [["João", "28"], ["Maria", "32"]]))
 console.log(contentToJsonData(["produto", "preço"], [["Arroz", "5.00"], ["Feijão", "7.50"]]))
 console.log(contentToJsonData(["Filme", "Diretor", "Ano", "Duração", "Gênero", "País de Origem", "Receita Bruta"], [["Parasita", "Bong Joon-ho", "2019", "132", "Drama", "Coréia do Sul", "258.8M"], ["Green Book", "Peter Farrelly", "2018", "130", "Drama", "EUA", "321.8M"]]))
+
+module.exports = { extract, csvToLines, linesToColumns, extractHeader, extractContent, rowToJSON, contentToJsonData }
